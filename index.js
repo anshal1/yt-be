4691b744df98b5e975c156048e7e3bb408ddd27f
amshal1 <panshal911@gmail.com>
2024-05-03 21:13:54 +0530

    test 2

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const PORT = process.env.PORT || 5000;
-const app = express();
-const allRoutes = require("./src/routes/index");
-const Connect = require("./src/Connect");
+const express = require('express')
+const PORT = process.env.PORT || 5000
+const app = express()
+const allRoutes = require('./src/routes/index')
+const Connect = require('./src/Connect')
 
-app.use(express.json());
+app.use(express.json())
 
-app.use("/", allRoutes);
+app.use('/', allRoutes)
 
 app.use((err, req, res, next) => {
-  res.status(err?.status).json({ error: err?.message, status: err?.status });
-  console.log("An Error Has Occured");
-  next();
-});
+  res.status(err?.status).json({ error: err?.message, status: err?.status })
+  console.log('An Error Has Occured')
+  next()
+})
 
 app.listen(PORT, () => {
-  console.log(`App Running On Port ${PORT}`);
-  Connect();
-});
+  console.log(`App Running On Port ${PORT}`)
+  Connect()
+})
--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,19 +1,19 @@
-const CatchErr = require('../../utils/CatchErr');
-const { userModel } = require('../models/index');
+const CatchErr = require('../../utils/CatchErr')
+const { userModel } = require('../models/index')
 
-var test = 100;
+var test = 100
 
 const getUser = CatchErr((req, res) => {
-  res.status(200).json(req.user);
-});
+  res.status(200).json(req.user)
+})
 
 const CreateUser = CatchErr(async (req, res) => {
-  const { body } = req;
-  const user = await userModel.create(body);
-  res.status(201).json(user);
-});
+  const { body } = req
+  const user = await userModel.create(body)
+  res.status(201).json(user)
+})
 
 module.exports = {
   getUser,
   CreateUser,
-};
+}
