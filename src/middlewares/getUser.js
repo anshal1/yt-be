b642f716e8373fddfe99127446d6bff52372f0e9
amshal1 <panshal911@gmail.com>
2024-05-04 08:29:10 +0530

    get user from jwt token added

--- src/middlewares/getUser.js
+++ src/middlewares/getUser.js
@@ -1,21 +1,27 @@
-const ApiError = require("../../utils/ApiError");
-const CatchErr = require("../../utils/CatchErr");
-const { userModel } = require("../models/index");
+const ApiError = require('../../utils/ApiError')
+const CatchErr = require('../../utils/CatchErr')
+const { userModel } = require('../models/index')
+const jwt = require('jsonwebtoken')
 
 const getUserById = (showPassword = false) =>
   CatchErr(async (req, res, next) => {
-    const id = req.header("token");
+    const id = req.header('token')
     if (!id) {
-      throw new ApiError("Please Login Or Register To Continue", 400);
+      throw new ApiError('Please Login Or Register To Continue', 400)
+    }
+    // eslint-disable-next-line no-undef
+    const userId = jwt.decode(id, process.env.SECRET)
+    if (!userId) {
+      throw new ApiError('Unaouthorized Action')
     }
     const user = await userModel
-      .findById(id)
-      .select(showPassword ? "" : "-password");
+      .findById(userId?.token)
+      .select(showPassword ? '' : '-password')
     if (!user) {
-      throw new ApiError("User Not Found", 404);
+      throw new ApiError('User Not Found', 404)
     }
-    req.user = user;
-    next();
-  });
+    req.user = user
+    next()
+  })
 
-module.exports = getUserById;
+module.exports = getUserById
