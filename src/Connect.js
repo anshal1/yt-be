d3935df352f07ed3092cf6949de01b9bf8dbf8c5
amshal1 <panshal911@gmail.com>
2024-05-04 08:04:23 +0530

    some more packages downloaded

--- src/Connect.js
+++ src/Connect.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 function Connect() {
-  mongoose.connect("mongodb://127.0.0.1:27017/yt").then(() => {
-    console.log("Database Connected");
-  });
+  mongoose.connect('mongodb://127.0.0.1:27017/yt').then(() => {
+    console.log('Database Connected')
+  })
 }
 
-module.exports = Connect;
+module.exports = Connect
